481ce0e951f4db2267813da74c0bce3941bf6264
yr1404 <yash06765@gmail.com>
2024-07-06 15:08:39 +0530

    added bottom icons

--- script.js
+++ script.js
@@ -1,29 +1,27 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const jb = document.querySelector('.jb-1');
-    const arrow = document.querySelector('.right-arrow');
-    const temp = document.querySelector('.temp');
-    let toggled = false;
+const jb = document.querySelector('.jb-1');
+const arrow = document.querySelector('.right-arrow');
+const temp = document.querySelector('.temp');
+let toggled = false;
 
-    jb.addEventListener('click', (event) => {
-      event.stopPropagation(); // Prevent click from bubbling up to the document
-      if (toggled) {
+jb.addEventListener('click', (event) => {
+    event.stopPropagation();
+    if (toggled) {
         jb.style.transform = "translateX(0px)";
         arrow.style.transform = "rotate(0deg)";
-        temp.classList.remove('hidden'); // Show .temp
-      } else {
+        temp.classList.remove('hidden');
+    } else {
         jb.style.transform = "translateX(600px)";
         arrow.style.transform = "rotate(180deg)";
-        temp.classList.add('hidden'); // Hide .temp
-      }
-      toggled = !toggled;
-    });
+        temp.classList.add('hidden');
+    }
+    toggled = !toggled;
+});
 
-    document.addEventListener('click', () => {
-      if (toggled) {
+document.addEventListener('click', () => {
+    if (toggled) {
         jb.style.transform = "translateX(0px)";
         arrow.style.transform = "rotate(0deg)";
-        temp.classList.remove('hidden'); // Show .temp
+        temp.classList.remove('hidden');
         toggled = false;
-      }
-    });
-  });
+    }
+});
